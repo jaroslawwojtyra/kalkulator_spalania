cd2ca6daa74aa512c65802f6ce42f3a3e7f1fd7d
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-20 16:27:41 +0100

    v.0.0.2  samochody + suwak

---
+++ script.js
@@ -0,0 +1,17 @@
+let hybrid = {
+  name : 'opel',
+  fuel : 50,
+  combustion_city : 8,
+  combustion_road : 5,
+};
+let petrol = {
+  name : 'seat',
+  combustion_city : 4,
+  combustion_road : 7,
+  fuel : 30,
+};
+let petrolPrice = 5;
+// Przygotować kalkulator porównawczy w zależności od
+// ilości km które przejeżdżamy w mieście i na trasie miesięcznie
+// Kalkulator oblicza ile pieniędzy wydamy na jeden samochód i
+// na drugi i pokazuje który jest bardziej opłacalny
