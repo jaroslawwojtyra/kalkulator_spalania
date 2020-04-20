7f31a3da81c77dfbca0fba1441ed5e20c9743ac1
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-04-20 14:17:43 +0200

    v. 0.3.2 - kontener + flex + img

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let hybrid = {
-  name : 'opel',
-  fuel : 50,
-  combustion_city : 8,
-  combustion_road : 5,
+  name: 'opel',
+  fuel: 50,
+  combustion_city: 8,
+  combustion_road: 5,
 };
 let petrol = {
-  name : 'seat',
-  combustion_city : 4,
-  combustion_road : 7,
-  fuel : 30,
+  name: 'seat',
+  combustion_city: 4,
+  combustion_road: 7,
+  fuel: 30,
 };
 let petrolPrice = 5;
 const sliderCity = document.getElementById('sliderCity');
@@ -17,23 +17,23 @@ const kmCity = document.getElementById('kmCity');
 const kmRoad = document.getElementById('kmRoad');
 
 function setCity() {
-    kmCity.value = sliderCity.value;
-  };
+  kmCity.value = sliderCity.value;
+};
+
 function setRoad() {
-     kmRoad.value = sliderRoad.value;
-  };
+  kmRoad.value = sliderRoad.value;
+};
 
 function setCitySlider() {
-    sliderCity.value = kmCity.value;
-  };
+  sliderCity.value = kmCity.value;
+};
+
 function setRoadSlider() {
-     sliderRoad.value = kmRoad.value;
-  };
+  sliderRoad.value = kmRoad.value;
+};
 
 sliderCity.addEventListener('click', setCity);
 sliderRoad.addEventListener('click', setRoad);
-kmCity.addEventListener('click', setCitySlider);
-kmRoad.addEventListener('click', setRoadSlider);
 kmCity.addEventListener('keyup', setCitySlider);
 kmRoad.addEventListener('keyup', setRoadSlider);
 
