d1f63b2e85b0b254bca2b7191d802e13164de900
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-06-28 12:08:33 +0200

    v. 0.4.1 - obliczanie kosztów

--- scripts/script.js
+++ scripts/script.js
@@ -15,6 +15,8 @@ const sliderCity = document.getElementById('sliderCity');
 const sliderRoad = document.getElementById('sliderRoad');
 const kmCity = document.getElementById('kmCity');
 const kmRoad = document.getElementById('kmRoad');
+const score = document.getElementById('score');
+const button = document.getElementById('button');
 
 function setCity() {
     kmCity.value = sliderCity.value;
@@ -30,12 +32,24 @@ function setRoadSlider() {
      sliderRoad.value = kmRoad.value;
   };
 
+function checkScore() {
+  let hybridCityScore = kmCity.value * hybrid.combustion_city
+  let hybridRoadScore = kmRoad.value * hybrid.combustion_road
+  let petrolCityScore = kmCity.value * petrol.combustion_city
+  let petrolRoadScore = kmRoad.value * petrol.combustion_road
+  let hybridScore = hybridCityScore + hybridRoadScore
+  let petrolScore = petrolCityScore + petrolRoadScore
+  console.log(hybridScore);
+  console.log(petrolScore);
+  }
+
 sliderCity.addEventListener('click', setCity);
 sliderRoad.addEventListener('click', setRoad);
 kmCity.addEventListener('click', setCitySlider);
 kmRoad.addEventListener('click', setRoadSlider);
 kmCity.addEventListener('keyup', setCitySlider);
 kmRoad.addEventListener('keyup', setRoadSlider);
+button.addEventListener('click', checkScore);
 
 setCity();
 setRoad();
